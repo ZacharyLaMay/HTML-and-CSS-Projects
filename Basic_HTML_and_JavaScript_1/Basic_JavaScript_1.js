87abd1987532ba9547d983c4b40c2fa69000ed10
ZacharyLaMay <143453707+ZacharyLaMay@users.noreply.github.com>
2023-09-25 17:26:46 -0400

    Recreated Repository

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,36 @@
+<<<<<<< HEAD
+<!DOCTYPE HTML>
+<html lang="en">
+<body>
+<script>
+alert("You are viewing an HTML document that is used for practice writing JavaScript.");
+</script>
+		
+<script>
+	function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
+</script>
+</body>
+=======
+<!DOCTYPE HTML>
+<html lang="en">
+<body>
+<script>
+alert("You are viewing an HTML document that is used for practice writing JavaScript.");
+</script>
+		
+<script>
+	function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
+</script>
+</body>
+>>>>>>> 6e57520b6dc84d88d7ebc63bb2185fe65e10bfc6
+</html>
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # HTML-and-CSS-Projects
- 
+This is a repository for the assignments and projects I completed during my tenure at The Tech Academy.
